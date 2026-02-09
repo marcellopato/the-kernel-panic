b3e86435fccc94474519bf4ed5632ceef446fb44
Marcello Pato <marcello.dantas@halai.com>
2026-02-09 18:53:28 -0300

    chore: remove gh-pages config and prepare for vercel

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	// Set base to repo name for GitHub Pages subfolder deployment
-	base: '/the-kernek-panic/',
 	build: {
 		outDir: 'dist',
 	}
